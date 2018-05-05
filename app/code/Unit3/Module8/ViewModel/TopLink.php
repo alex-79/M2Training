1bb223425cca034b1c5c84a0aec47754b48f470c
Alexander Bobylev <alexander.bobylev@gmail.com>
2018-05-05 08:53:07 +0300

    Unit Three - Module 8. Layout XML: Loading & Rendering - final

---
+++ app/code/Unit3/Module8/ViewModel/TopLink.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Unit3\Module8\ViewModel;
+
+class TopLink implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+    public function __construct()
+    {
+    }
+}
+    
+
